c15f3c92dde97e85014a156723e1d2149043d920
Francuski Miroslav <francuski.miroslav@gmail.com>
2020-04-22 00:12:32 +0200

    Updates

---
+++ precache-manifest.20d6ece2f250d90a4501eee2e1e9cc02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3942e8f5c3e5226c32744394c9c2efe0",
+    "url": "/COVID-19trackerAppReactJS/index.html"
+  },
+  {
+    "revision": "8c86ea22443c75f056e1",
+    "url": "/COVID-19trackerAppReactJS/static/css/main.e9e3b1bc.chunk.css"
+  },
+  {
+    "revision": "b85b8ce39d9559f8b865",
+    "url": "/COVID-19trackerAppReactJS/static/js/2.1c80eb02.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19trackerAppReactJS/static/js/2.1c80eb02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c86ea22443c75f056e1",
+    "url": "/COVID-19trackerAppReactJS/static/js/main.5363d698.chunk.js"
+  },
+  {
+    "revision": "594bd89f747354dd843b",
+    "url": "/COVID-19trackerAppReactJS/static/js/runtime-main.5c1db29a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19trackerAppReactJS/static/media/image.d7265326.png"
+  }
+]);
