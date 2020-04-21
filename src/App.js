d9f873b506436a8d892eea55a1c9c99e018f8005
Francuski Miroslav <francuski.miroslav@gmail.com>
2020-04-21 22:48:54 +0200

    needs styling and some fixes

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import CountryPicker from "./components/CountryPicker/CountryPicker";
 import styles from "./App.module.css";
 import { fetchData } from "./api";
 
+import imageLogo from "./images/image.png";
+
 class App extends React.Component {
   state = {
     data: {},
+    country: "",
   };
 
   async componentDidMount() {
@@ -17,14 +20,21 @@ class App extends React.Component {
     this.setState({ data: fetchedData });
   }
 
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+
+    this.setState({ data: fetchedData, country: country });
+  };
+
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
 
     return (
       <div className={styles.container}>
+        <img className={styles.image} src={imageLogo} alt="Logo-img" />
         <Cards data={data} />
-        <CountryPicker />
-        <Chart />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
       </div>
     );
   }
